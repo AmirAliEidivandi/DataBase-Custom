4b32a3664254bb37416c4b659eb8b3fcfc7108ad
AmirAliEidivandi <amirah648887@gmail.com>
2022-11-10 19:58:32 +0330

    create new command

--- parsers/insert.js
+++ parsers/insert.js
@@ -1,13 +1,10 @@
 const InsertCommand = require("../commands/InsertCommand");
 const safeParseJSON = require("../utils/safeParseJSON");
 
-// INSERT { "a": 1 } INTO tableName
-
 const INSERT_COMMAND = "INSERT";
 const BEFORE_TABLE_COMMAND = "INTO";
 const REGEX = new RegExp(`${INSERT_COMMAND}\\s+(?<record>{.*})\\s+${BEFORE_TABLE_COMMAND}\\s+(?<tableName>\\S+)`);
 
-// SELECT * FROM test
 function parseInsertCommand(commandString) {
     const regexMatch = commandString.match(REGEX);
     if (regexMatch == null) return;
--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-const insertParser = require("./parsers/insert");
-
-const insertCommand = insertParser('INSERT { "a": 1, "b": { "c": 3 } } INTO test');
-
-async function main() {
-    console.log(await insertCommand.perform());
-}
-
-main();
-// 1. Get user input
-// 1.2 Choose our parser
-// 2. Parse input
-// 3. Excute input
-// 4. Return data
-// 5. Repeat
-
-// SELECT * FROM test
-// DELETE FROM test
+console.log("hello world");
